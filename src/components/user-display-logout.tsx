de13fc0965b5dbc5210fbf02ada9ca9680870f49
Albert Chang <a89529294@hotmail.com>
2024-04-19 14:09:05 +0800

    add table

--- src/components/user-display-logout.tsx
+++ src/components/user-display-logout.tsx
@@ -6,12 +6,15 @@ export function UserDisplayLogout() {
   const { user, logout } = useAuth();
   const navigate = useNavigate();
 
-  if (!user) return navigate("/login");
+  if (!user) {
+    navigate("/login");
+    return null;
+  }
 
   return (
-    <div className="border-r-light-blue absolute bottom-0 right-0 top-0 flex h-full border-r-[10px] bg-light-gray py-2.5 pl-2.5">
+    <div className="absolute bottom-0 right-0 top-0 flex h-full border-r-[10px] border-r-light-blue bg-light-gray py-2.5 pl-2.5">
       <IconButtonBorderLess icon="pfp">{user.name}</IconButtonBorderLess>
-      <div className="border-line-gray h-full border-r" />
+      <div className="h-full border-r border-line-gray" />
       <IconButtonBorderLess icon="leave" onClick={logout}>
         登出
       </IconButtonBorderLess>
--- src/pages/personnel-data-management.tsx
+++ src/pages/personnel-data-management.tsx
@@ -1,16 +1,43 @@
+import { columns } from "@/components/personnel-data-management-table/columns";
+import { DataTable } from "@/components/personnel-data-management-table/data-table";
 import { IconButton } from "@/components/ui/button";
 import { UserDisplayLogout } from "@/components/user-display-logout";
+
+type Payment = {
+  id: string;
+  amount: number;
+  status: "pending" | "processing" | "success" | "failed";
+  email: string;
+};
+
+export const payments: Payment[] = [
+  {
+    id: "728ed52f",
+    amount: 100,
+    status: "pending",
+    email: "m@example.com",
+  },
+  {
+    id: "489e1d42",
+    amount: 125,
+    status: "processing",
+    email: "example@gmail.com",
+  },
+  // ...
+];
 
 export default function PersonnelDataManagement() {
   return (
     <div className="relative flex flex-col gap-2.5 px-2.5">
-      <header className="flex h-20 items-end gap-2.5">
+      <header className="relative flex h-20 items-end gap-2.5">
         <IconButton icon="plus">新增人員</IconButton>
         <IconButton icon="magnifyingGlass" />
 
         <UserDisplayLogout />
       </header>
-      <div></div>
+      <div>
+        <DataTable columns={columns} data={payments} />
+      </div>
     </div>
   );
 }
