db5f654b76ffdfa010824ec6d8f1487a77e2cbf9
Albert Chang <a89529294@hotmail.com>
2025-04-15 09:26:55 +0800

    update payment status

--- src/pages/indoor-simulator/appointment-management/data-table/row.tsx
+++ src/pages/indoor-simulator/appointment-management/data-table/row.tsx
@@ -273,17 +273,19 @@ export function Row({ row }: { row: RowModel<Appointment>["rows"][number] }) {
                   付款方式
                 </p>
                 <p className="text-secondary-purple">
-                  {row.original.order?.paymentMethod ?? "點數"}
+                  {row.original.order?.paymentMethod ?? ""}
                 </p>
               </div>
               <div className="w-14">
                 <p className="text-sm font-medium text-secondary-dark">狀態</p>
                 <p className="text-secondary-purple">
-                  {row.original.order?.status === "success"
-                    ? "成功"
-                    : row.original.order?.status === "pending"
-                      ? "待付款"
-                      : row.original.order?.status}
+                  {row.original.order?.paymentMethod === null
+                    ? "未付款"
+                    : row.original.order?.status === "success"
+                      ? "成功"
+                      : row.original.order?.status === "pending"
+                        ? "待付款"
+                        : row.original.order?.status}
                 </p>
               </div>
             </div>
--- src/pages/indoor-simulator/appointment-management/data-table/table.tsx
+++ src/pages/indoor-simulator/appointment-management/data-table/table.tsx
@@ -103,7 +103,7 @@ export function DataTable<TData extends Appointment, TValue>({
             電話: r.original.appUser?.phone,
             開始時間: r.original.startTime,
             結束時間: r.original.endTime,
-            付款方式: r.original.order?.paymentMethod ?? "點數",
+            付款方式: r.original.order?.paymentMethod ?? "",
             狀態: r.original.status,
             原訂單金額: originAmount,
             折數: percentOff,
